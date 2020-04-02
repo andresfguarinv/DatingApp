4f4a262e2c3819aa2bbcf830d8e90e0fc2b3fd57
Andres Guarin <andres.guarin@simplexity.com.co>
2020-04-02 12:18:13 -0500

    Validation with DataAnotations

--- DatingApp.Api/Dtos/UserForRegisterDto.cs
+++ DatingApp.Api/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.Api.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password etwenn 4 and 8 character")]
         public string Password { get; set; }
     }
 }
