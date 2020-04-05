a4bae59eaa16e14ab9f9f4a4ec34c9515a2fd55a
Andres Guarin <andres.guarin@simplexity.com.co>
2020-04-05 17:55:32 -0500

    Authorize App

--- DatingApp.Api/Controllers/ValuesController.cs
+++ DatingApp.Api/Controllers/ValuesController.cs
@@ -1,11 +1,13 @@
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.Api.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -17,7 +19,6 @@ namespace DatingApp.API.Controllers
             context = dataContext;
         }
 
-        // GET api/values
         [HttpGet]
         public async Task<ActionResult> GetValues()
         {
@@ -25,7 +26,8 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
-        // GET api/values/5
+
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<ActionResult> GetValue(int id)
         {
@@ -33,20 +35,17 @@ namespace DatingApp.API.Controllers
             return Ok(value);
         }
 
-        // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
             
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
