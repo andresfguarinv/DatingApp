53404bbd9436a86956a1d59759b6ed2841b97225
Andres Guarin <andres.guarin@simplexity.com.co>
2020-04-21 17:40:22 -0500

    Error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in succesfully');
     }, error => {
-      console.error('Failed to login');
+      console.error(error);
     });
   }
 
