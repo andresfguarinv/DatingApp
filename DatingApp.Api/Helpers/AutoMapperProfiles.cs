a8f59aa872802d2a1511d936fe28dd5a719fb9fd
Andres Guarin <andres.guarin@simplexity.com.co>
2020-06-05 07:43:05 -0500

    Calculate fields with maper

--- DatingApp.Api/Helpers/AutoMapperProfiles.cs
+++ DatingApp.Api/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using DatingApp.Api.Dtos;
 using DatingApp.Api.Models;
@@ -8,8 +9,18 @@ namespace DatingApp.Api.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailedDto>();
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
