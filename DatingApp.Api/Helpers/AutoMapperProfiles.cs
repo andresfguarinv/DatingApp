42652bcfc31ca6e94761dc8f1d2896f44f4a8374
Andres Guarin <andres.guarin@simplexity.com.co>
2020-06-05 07:10:34 -0500

    Fix return information navegation propertys

--- DatingApp.Api/Helpers/AutoMapperProfiles.cs
+++ DatingApp.Api/Helpers/AutoMapperProfiles.cs
@@ -10,6 +10,7 @@ namespace DatingApp.Api.Helpers
         {
             CreateMap<User, UserForListDto>();
             CreateMap<User, UserForDetailedDto>();
+            CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
 }
