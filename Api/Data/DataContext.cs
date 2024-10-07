6d0def5cf5fc6d3a0dae2cc1a75a393e6199a46a
Andres Guarin <andresguarin10@hotmail.com>
2024-10-06 19:15:12 -0500

    Building a skeleton api

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
