ea757ed865ca44b770d979315f83e390fca9bfd8
Andres Guarin <andresfguarinv@gmail.com>
2025-02-20 09:13:14 -0500

    Adding the likes feature

---
+++ Api/Entities/UserLike.cs
@@ -0,0 +1,10 @@
+namespace Api.Entities
+{
+    public class UserLike
+    {
+        public AppUser SourceUser { get; set; } = null!;
+        public int SourceUserId { get; set; }
+        public AppUser TargetUser { get; set; } = null!;
+        public int TargetUserId { get; set; }
+    }
+}
--- Client/src/app/lists/lists.component.ts
+++ Client/src/app/lists/lists.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { LikesService } from '../services/likes.service';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ButtonsModule, MemberCardComponent, PaginationModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit, OnDestroy {
 
+  likeService = inject(LikesService);
+  predicated = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  getTitle() {
+    switch (this.predicated) {
+      case 'liked': return 'Members you like'
+      case 'likedBy': return 'Member who liked you'
+      default: return 'Mutual'
+    }
+  }
+
+  loadLikes() {
+    this.likeService.getLikes(this.predicated, this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.likeService.paginatedResult.set(null);
+  }
 }
